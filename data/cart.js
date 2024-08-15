6db9760aac05344b91a336c2bab13072c081d09d
Alisa Guo <alisa.jingyi.guo@gmail.com>
2024-08-15 17:31:38 -0400

    Save and Load cart from localStorage

--- data/cart.js
+++ data/cart.js
@@ -1,10 +1,23 @@
-export let cart = [{
-    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-    quantity: 2,
-}, {
-    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    quantity: 1,
-}];
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+
+if (!cart) {
+    cart = [{
+        productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+        quantity: 2,
+    }, {
+        productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+        quantity: 1,
+    }];
+}
+
+function saveToStorage() {
+    //setItem() takes two strings
+        //first: name of what you wanna save
+        //second: data you wanna save
+    //remember localStorage can only save strings
+    localStorage.setItem('cart', JSON.stringify(cart));
+};
 
 export function addToCart(productId) {
     let added = false;
@@ -22,6 +35,7 @@ export function addToCart(productId) {
             quantity: 1
         });
     }
+    saveToStorage();
   }
 
 export function deleteFromCart(matchingProductId) {
@@ -34,4 +48,5 @@ export function deleteFromCart(matchingProductId) {
     });
 
     cart = newCart;
+    saveToStorage();
 }
