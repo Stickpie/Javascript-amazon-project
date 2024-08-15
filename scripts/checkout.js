303c17f6203923d3fdae5fa3488d4f6ddf84a7b6
Alisa Guo <alisa.jingyi.guo@gmail.com>
2024-08-15 16:27:21 -0400

    Delete products from HTML when clicking delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -17,7 +17,7 @@ cart.forEach((cartItem)=>{
     });
 
     cartSummaryHTML += `
-    <div class="cart-item-container">
+    <div class="cart-item-container js-cart-item-container-${productId}">
         <div class="delivery-date">
             Delivery date: Tuesday, June 21
         </div>
@@ -101,6 +101,8 @@ document.querySelectorAll('.js-delete-link').forEach((link)=>{
     link.addEventListener('click', () => {
         const productID = link.dataset.productId;
         deleteFromCart(productID);
-        
-    })
+
+        const container = document.querySelector(`.js-cart-item-container-${productID}`);
+        container.remove();
+    });
 });
