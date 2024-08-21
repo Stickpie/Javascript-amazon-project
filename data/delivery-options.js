d2806cd9b27c140b3c3b81970f6cb883fad21516
Alisa Guo <alisa.jingyi.guo@gmail.com>
2024-08-20 23:57:16 -0400

    Save date and generate HTML for delivery options

---
+++ data/delivery-options.js
@@ -0,0 +1,14 @@
+export const deliveryOptions = [{
+    id: '1',
+    deliveryDays: 7,
+    priceCents: 0
+    }, {
+    id: '2',
+    deliveryDays: 3,
+    priceCents: 399
+    }, {
+    id: '3',
+    deliveryDays: 0,
+    priceCents: 599
+    }
+];
